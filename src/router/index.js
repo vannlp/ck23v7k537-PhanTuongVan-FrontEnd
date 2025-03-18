40f7f3f5df4869c7a5803799a5f7aef4d90dead5
phanvan0322 <phanvan0322@gmail.com>
2025-03-18 16:47:11 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,17 @@ const routes = [
         component: ContactBook,
     },
     {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
+    {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
